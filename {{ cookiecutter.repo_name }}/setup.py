b105aefca4017daf75d529319a2f2226836c3b59
Cassie Borish <cassieborish@gmail.com>
2018-11-28 09:15:18 -0800

    modified for personal use

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,10 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ cookiecutter.pkg_name }}',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
-    license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
 )
